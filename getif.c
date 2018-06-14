ec0712023a6a3683626fa24717e9d28d0cca1cdd
shawrylk <shawry.lk@gmail.com>
2018-06-14 16:43:09 +0700

    fourth commit

--- getif.c
+++ getif.c
@@ -1,33 +1,15 @@
+#include <stdio.h>      
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <stdio.h>
-#include <netinet/in.h>
+#include <ifaddrs.h>
+#include <netinet/in.h> 
+#include <string.h> 
 #include <arpa/inet.h>
-#include <unistd.h>
-#include <stdlib.h>
 
-int main()
-{
-	int sockfd;
-	int len;
-	struct sockaddr_in address;
-	int result;
-	char ch = 'A';
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = inet_addr("127.0.1.1");
-	address.sin_port = htons(9734);
-	len = sizeof(address);
-	result = connect(sockfd, (struct sockaddr *)&address, len);
-	if (result == -1)
-	{
-		perror("oops: client");
-		exit(1);
-	}
-	struct ifaddrs * ifAddrStruct=NULL;
+int main (int argc, const char * argv[]) {
+    struct ifaddrs * ifAddrStruct=NULL;
     struct ifaddrs * ifa=NULL;
     void * tmpAddrPtr=NULL;
-	char buff[126];
+
     getifaddrs(&ifAddrStruct);
 
     for (ifa = ifAddrStruct; ifa != NULL; ifa = ifa->ifa_next) {
@@ -39,23 +21,15 @@ int main()
             tmpAddrPtr=&((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
             char addressBuffer[INET_ADDRSTRLEN];
             inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
-			stpcpy(buff, ifa->ifa_name);
-            write(sockfd, &buff, strlen(buff));
-			stpcpy(buff, addressBuffer);
-            write(sockfd, &buff, strlen(buff));
+            printf("%s IP Address %s\n", ifa->ifa_name, addressBuffer); 
         } else if (ifa->ifa_addr->sa_family == AF_INET6) { // check it is IP6
             // is a valid IP6 Address
             tmpAddrPtr=&((struct sockaddr_in6 *)ifa->ifa_addr)->sin6_addr;
             char addressBuffer[INET6_ADDRSTRLEN];
             inet_ntop(AF_INET6, tmpAddrPtr, addressBuffer, INET6_ADDRSTRLEN);
-            stpcpy(buff, ifa->ifa_name);
-            write(sockfd, &buff, strlen(buff));
-			stpcpy(buff, addressBuffer);
-            write(sockfd, &buff, strlen(buff));
+            printf("%s IP Address %s\n", ifa->ifa_name, addressBuffer); 
         } 
     }
     if (ifAddrStruct!=NULL) freeifaddrs(ifAddrStruct);
     return 0;
-	close(sockfd);
-	exit(0);
 }
